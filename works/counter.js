7facb9cf50c44647e96a75574f8592145a832400
しゅーまい <62363188+Shumaislime@users.noreply.github.com>
2020-06-26 23:01:05 +0900

    test

--- works/counter.js
+++ works/counter.js
@@ -4,6 +4,7 @@ const countUp = () => {
   const countElement = document.querySelector('#count')
   countElement.innerText = '回数: ' + count
 }
+
 const countReset = () => {
   count = 0
   const countElement = document.querySelector('#count')
